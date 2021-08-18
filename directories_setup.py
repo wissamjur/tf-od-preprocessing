055b35973d31dfd21604bf05a2487b6b536d6a99
wissamjur <44833637+wissamjur@users.noreply.github.com>
2021-08-18 16:51:53 +0300

    Update directories_setup.py
    fixed dict syntax error

--- directories_setup.py
+++ directories_setup.py
@@ -5,19 +5,19 @@ def create_directories(PROJECT_NAME, CUSTOM_MODEL_NAME):
   # project environment set-up
   paths = {
     # directories
-    WORKSPACE_PATH: os.path.join('TensorFlow', 'workspace'),
-    PROJECT_PATH: os.path.join('TensorFlow', 'workspace', PROJECT_NAME),
-    APIMODEL_PATH: os.path.join('TensorFlow','models'),
-    PROTOC_PATH: os.path.join('TensorFlow', 'prerequisites', 'protoc'),
-    SCRIPTS_PATH: os.path.join('TensorFlow', 'scripts'),
-    ANNOTATIONS_PATH: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'annotations'),
+    'WORKSPACE_PATH': os.path.join('TensorFlow', 'workspace'),
+    'PROJECT_PATH': os.path.join('TensorFlow', 'workspace', PROJECT_NAME),
+    'APIMODEL_PATH': os.path.join('TensorFlow','models'),
+    'PROTOC_PATH': os.path.join('TensorFlow', 'prerequisites', 'protoc'),
+    'SCRIPTS_PATH': os.path.join('TensorFlow', 'scripts'),
+    'ANNOTATIONS_PATH': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'annotations'),
    
     # model information
-    PRETRAINED_MODEL_PATH: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'pre-trained-models'),
-    LABELMAP: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'annotations', 'label_map.pbtxt'),
-    CHECKPOINT_PATH: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'models', CUSTOM_MODEL_NAME),
-    PIPELINE_CONFIG: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    EXPORT_PATH: os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'exported_models')
+    'PRETRAINED_MODEL_PATH': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'pre-trained-models'),
+    'LABELMAP': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'annotations', 'label_map.pbtxt'),
+    'CHECKPOINT_PATH': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'models', CUSTOM_MODEL_NAME),
+    'PIPELINE_CONFIG': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'EXPORT_PATH': os.path.join('TensorFlow', 'workspace', PROJECT_NAME, 'exported_models')
   }
  
   try:
