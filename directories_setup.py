3e1dd576745256f485e31578b1ed82216e2fe960
wissamjur <44833637+wissamjur@users.noreply.github.com>
2021-08-18 16:44:12 +0300

    Update directories_setup.py
    fixed syntax error

--- directories_setup.py
+++ directories_setup.py
@@ -5,19 +5,19 @@ def create_directories(PROJECT_NAME, CUSTOM_MODEL_NAME):
   # project environment set-up
   paths = {
     # directories
-    WORKSPACE_PATH = os.path.join('TensorFlow', 'workspace'),
-    PROJECT_PATH = os.path.join(WORKSPACE_PATH, PROJECT_NAME),
-    APIMODEL_PATH = os.path.join('TensorFlow','models'),
-    PROTOC_PATH = os.path.join('TensorFlow', 'prerequisites', 'protoc'),
-    SCRIPTS_PATH = os.path.join('TensorFlow', 'scripts'),
-    ANNOTATIONS_PATH = os.path.join(PROJECT_PATH, 'annotations'),
+    WORKSPACE_PATH: os.path.join('TensorFlow', 'workspace'),
+    PROJECT_PATH: os.path.join(WORKSPACE_PATH, PROJECT_NAME),
+    APIMODEL_PATH: os.path.join('TensorFlow','models'),
+    PROTOC_PATH: os.path.join('TensorFlow', 'prerequisites', 'protoc'),
+    SCRIPTS_PATH: os.path.join('TensorFlow', 'scripts'),
+    ANNOTATIONS_PATH: os.path.join(PROJECT_PATH, 'annotations'),
    
     # model information
-    PRETRAINED_MODEL_PATH = os.path.join(PROJECT_PATH, 'pre-trained-models'),
-    LABELMAP = os.path.join(PROJECT_PATH, 'annotations', 'label_map.pbtxt'),
-    CHECKPOINT_PATH = os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME),
-    PIPELINE_CONFIG = os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    EXPORT_PATH = os.path.join(PROJECT_PATH, 'exported_models')
+    PRETRAINED_MODEL_PATH: os.path.join(PROJECT_PATH, 'pre-trained-models'),
+    LABELMAP: os.path.join(PROJECT_PATH, 'annotations', 'label_map.pbtxt'),
+    CHECKPOINT_PATH: os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME),
+    PIPELINE_CONFIG: os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    EXPORT_PATH: os.path.join(PROJECT_PATH, 'exported_models')
   }
  
   try:
