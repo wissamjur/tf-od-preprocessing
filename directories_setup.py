5fd2130a93ceead81aad4e17b13e241349d0b91f
wissamjur <44833637+wissamjur@users.noreply.github.com>
2021-08-18 14:46:16 +0300

    Create directories_setup.py
    helper function that creates all the necessary environment directories

---
+++ directories_setup.py
@@ -0,0 +1,31 @@
+import os
+
+def create_directories(PROJECT_NAME, CUSTOM_MODEL_NAME):
+  
+  # project environment set-up
+  paths = {
+    # directories
+    WORKSPACE_PATH = os.path.join('TensorFlow', 'workspace'),
+    PROJECT_PATH = os.path.join(WORKSPACE_PATH, PROJECT_NAME),
+    APIMODEL_PATH = os.path.join('TensorFlow','models'),
+    PROTOC_PATH = os.path.join('TensorFlow', 'prerequisites', 'protoc'),
+    SCRIPTS_PATH = os.path.join('TensorFlow', 'scripts'),
+    ANNOTATIONS_PATH = os.path.join(PROJECT_PATH, 'annotations'),
+   
+    # model information
+    PRETRAINED_MODEL_PATH = os.path.join(PROJECT_PATH, 'pre-trained-models'),
+    LABELMAP = os.path.join(PROJECT_PATH, 'annotations', 'label_map.pbtxt'),
+    CHECKPOINT_PATH = os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME),
+    PIPELINE_CONFIG = os.path.join(PROJECT_PATH, 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    EXPORT_PATH = os.path.join(PROJECT_PATH, 'exported_models')
+  }
+ 
+  try:
+    for path in paths.values():
+      os.makedirs(path, exist_ok=True)
+
+    print("Successfully created environment directories")
+    return paths
+
+  except:
+    print("Error creating the environment directories")
