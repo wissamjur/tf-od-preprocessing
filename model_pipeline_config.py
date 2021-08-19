fd5fcceab6c04dcb46ed934da6552ed8285953af
wissamjur <44833637+wissamjur@users.noreply.github.com>
2021-08-19 14:34:02 +0300

    remove unnecessary function

--- model_pipeline_config.py
+++ model_pipeline_config.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 from google.protobuf import text_format
-from object_detection.utils import config_util
 from object_detection.protos import pipeline_pb2
-
-# (optional) - view the config file details
-config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
 
 def read_config(PIPELINE_CONFIG):
   pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
