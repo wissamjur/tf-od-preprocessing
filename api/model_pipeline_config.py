a8e67d56fa7913a610ba7c77313d812dcaa99e29
wissamjur <44833637+wissamjur@users.noreply.github.com>
2021-08-27 09:12:34 +0300

    Added a method to modify pipeline in this script

--- api/model_pipeline_config.py
+++ api/model_pipeline_config.py
@@ -13,3 +13,19 @@ def write_config(PIPELINE_CONFIG, pipeline_config):
     config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
     with tf.io.gfile.GFile(PIPELINE_CONFIG, "wb") as f:                                                                                                                                                                                                                       
         f.write(config_text)
+        
+def modify_config(PIPELINE_CONFIG, labels, TRAIN_BATCH_SIZE, paths, files):
+  pipeline_config = read_config(PIPELINE_CONFIG)
+
+  # pipeline configuration settings
+  pipeline_config.model.ssd.num_classes = len(labels)
+  pipeline_config.train_config.batch_size = TRAIN_BATCH_SIZE
+  pipeline_config.eval_config.batch_size = 1
+  pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
+  pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+  pipeline_config.train_input_reader.label_map_path= files['LABELMAP']
+  pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATIONS_PATH'], 'train.record')]
+  pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
+  pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATIONS_PATH'], 'test.record')]
+
+  write_config(PIPELINE_CONFIG, pipeline_config)
